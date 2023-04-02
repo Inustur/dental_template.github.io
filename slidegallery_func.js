af8ef97479c252932cf8a887741f1ef6f3d9a908
Vinh <100789255+Inustur@users.noreply.github.com>
2023-04-01 20:47:51 -0600

    Public Dental Website 1.0
    publicing dental website

---
+++ slidegallery_func.js
@@ -0,0 +1,36 @@
+// Name: Vinh Tien Pham
+// NSID: tip434
+// Course: CMPT_281(01)
+// Student number: 11290849
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next and previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Manage and Showing current slide
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
